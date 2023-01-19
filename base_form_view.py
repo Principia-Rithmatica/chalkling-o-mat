2f5a0d4fa5ce6c44d80f34f11ccd99629977d637
Florian Reinhold <kani@kemomi.de>
2023-01-19 15:48:11 +0100

    Fixed drawing area.

--- base_form_view.py
+++ base_form_view.py
@@ -28,9 +28,13 @@ class BaseFormView:
         event_dispatcher.listen(self.process_events)
         event_dispatcher.listen(self.switch_edit, self.edit_button)
 
+        self.form_surface = pygame.Surface((512, 512))
+
     def draw(self, screen: Surface):
+        self.form_surface.fill((30, 30, 30))
         if self.current_base_form is not None:
-            self.current_base_form.draw(screen)
+            self.current_base_form.draw(self.form_surface)
+        screen.blit(self.form_surface, (10, 10))
 
     def switch_edit(self, event):
         if event.type == pygame_gui.UI_BUTTON_PRESSED:
@@ -39,13 +43,15 @@ class BaseFormView:
             return True
 
     def process_events(self, event: Event):
-        if self.enable_edit:
-            if event.type == pygame.MOUSEBUTTONUP and event.button == LEFT_MOUSE_BUTTON:
-                self.add_point()
-                return True
-            if event.type == pygame.MOUSEBUTTONUP and event.button == RIGHT_MOUSE_BUTTON:
-                self.delete_point()
-                return True
+        if self.enable_edit and event.type == pygame.MOUSEBUTTONUP:
+            surf_rect = self.form_surface.get_rect()
+            if surf_rect.collidepoint(event.pos):
+                if event.button == LEFT_MOUSE_BUTTON:
+                    self.add_point()
+                    return True
+                if event.button == RIGHT_MOUSE_BUTTON:
+                    self.delete_point()
+                    return True
         return False
 
     def add_point(self):
